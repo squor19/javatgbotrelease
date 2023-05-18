e4967e09850fee767a7b11ca192e2c42d43bd5df
Maksym Martynenko <maksym.martynenko@pollub.edu.pl>
2023-05-19 01:39:01 +0200

    Initial commit

---
+++ src/main/java/com/github/squor22/javatgbot/JavaTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.squor22.javatgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/github/squor22/javatgbot/bot/JavaTgBot.java
@@ -0,0 +1,8 @@
+package com.github.squor22.javatgbot.bot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JavaTgBot {
+    
+}
