ed6e25c359dd553e237f323a4cedf68f08bb71c8
maksy <mak.martynenko@gmail.com>
2023-05-21 23:17:16 +0200

    finally!

--- src/main/java/com/github/squor22/javatgbot/bot/JavaTgBot.java
+++ src/main/java/com/github/squor22/javatgbot/bot/JavaTgBot.java
@@ -1,8 +1,57 @@
 package com.github.squor22.javatgbot.bot;
 
+import com.github.squor22.javatgbot.bot.config.BotConfig;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
-public class JavaTgBot {
-    
+public class JavaTgBot extends TelegramLongPollingBot{
+
+    final BotConfig config;
+
+    public JavaTgBot(BotConfig config) {
+        super(config.getBotToken());
+        this.config = config;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            Long chatId = update.getMessage().getChatId();
+
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                case "homie":
+                    sendMessage(chatId, "wassup");
+                default:
+            }
+        }
+    }
+
+    private void startCommandReceived(Long chatId, String firstName) {
+        String message = "Hello, " + firstName + "!";
+        sendMessage(chatId, message);
+    }
+
+    private void sendMessage(Long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/com/github/squor22/javatgbot/bot/config/BotInit.java
@@ -0,0 +1,27 @@
+package com.github.squor22.javatgbot.bot.config;
+
+import com.github.squor22.javatgbot.bot.JavaTgBot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Component
+public class BotInit {
+
+    @Autowired
+    JavaTgBot bot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(bot);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
